7693a1da3613c4a0e8b3810942412770c74949e1
SatyaHarish9 <harishgvt@gmail.com>
2020-04-12 16:06:57 -0400

    random-password-generator-python

--- random_password_generator.py
+++ random_password_generator.py
@@ -6,6 +6,8 @@ password = []
 
 for each in range(pass_length):
     password.append(choice(valid_chars_for_password))
-
+    
+# we are using join operation on the list to convert the password to string
 random_password = "".join(password)
 print(random_password)
+
